9c6237ea8200e1113b3fe2c653c93bfdca338ac9
parkj22 <jinyoung.park@vanderbilt.edu>
2022-07-16 02:53:23 +0900

    header component for all screens

---
+++ components/Header.js
@@ -0,0 +1,53 @@
+import { View, Image, Text, Pressable, TouchableOpacity } from "react-native";
+import React from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+const Header = ({ screenName }) => {
+  // TODO: Implement press handlers
+  const onProfilePress = () => {};
+  const onCalendarPress = () => {};
+  const onNotificationsPress = () => {};
+
+  return (
+    <View className="flex flex-row items-center px-4 py-1">
+      <Pressable onPress={onProfilePress}>
+        <Image
+          source={{
+            // TODO: Replace this temporary user profile pic
+            uri: "https://lh3.googleusercontent.com/a/AItbvmmUHkzpj1JAdkzyOabjy5bGUE-24arb__Z-Mga8=s96-c",
+          }}
+          className="h-8 w-8 rounded-full"
+        />
+      </Pressable>
+      <View className="flex-1">
+        {screenName == "Home" && (
+          <View className="flex flex-row justify-end space-x-4">
+            <Pressable onPress={onCalendarPress}>
+              <TouchableOpacity>
+                <View className="flex flex-row items-center space-x-1">
+                  <Text className="text-base">July</Text>
+                  <Ionicons name="calendar-outline" size="30" color="#00034C" />
+                </View>
+              </TouchableOpacity>
+            </Pressable>
+            <Pressable onPress={onNotificationsPress}>
+              <TouchableOpacity>
+                <Ionicons
+                  name="notifications-outline"
+                  size="30"
+                  color="#00034C"
+                />
+              </TouchableOpacity>
+            </Pressable>
+          </View>
+        )}
+
+        {/* Customize Header component for each screen */}
+        {screenName == "Insights" && <View></View>}
+        {screenName == "Community" && <View></View>}
+      </View>
+    </View>
+  );
+};
+
+export default Header;
